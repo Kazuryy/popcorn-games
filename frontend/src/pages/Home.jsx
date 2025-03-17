7e4f30c0b46bac778b4386bd39809150c2305c20
Kazuryy <luc.jacques72@gmail.com>
2025-03-17 10:50:00 +0100

    New images in assets

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Card from '../components/Card';
 import Page from '../components/Page';
+import imagetunnel from '../assets/tunnel.png';
+import imagemots from '../assets/les-mots-interdits.png';
 
 function Home() {
   return (
@@ -19,14 +21,14 @@ function Home() {
         <hr className="my-8 border-gray-300" />
         <div className="flex flex-wrap gap-4 justify-center">
           <Card
-            image="https://korben.info/webtunnel-le-nouveau-pont-tor-deguise-en-trafic-web-pour-contourner-la-censure/manu23_web_tunnels_to_avoid_censorship_d72931de-dc11-4447-8692-d5fe3891b868.webp"
+            image={imagetunnel}
             title="Le Tunnel"
             description="Trouve la bonne réponse et choisis entre prendre tes points et continuer de trouver les réponses dans ton tunnel."
             buttonText="Jouer"
             to="/tunnel"
           />
           <Card
-            image="https://www.radiofrance.fr/s3/cruiser-production/2020/12/0943738d-0301-4ddb-a8a7-c442e2bcccb0/1200x680_gettyimages-1164537342.webp"
+            image={imagemots}
             title="Les Mots Interdits"
             description="Trouve le mot de ton partenaire sans dire l'un des mots interdits."
             buttonText="Jouer"
